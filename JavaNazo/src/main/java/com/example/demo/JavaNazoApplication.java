458f7d964ac9ccc2bf08950e9cb7db2878835d6e
nishino <nishino@asojuku.ac.jp>
2023-07-07 14:54:57 +0900

    first

---
+++ JavaNazo/src/main/java/com/example/demo/JavaNazoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNazoApplication.class, args);
+	}
+
+}
